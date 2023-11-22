6fbe7b9121f81ed81e184a83a91dd92aa87ef525
Palychchempion <148390214+Palychchempion@users.noreply.github.com>
2023-11-22 23:40:45 +0300

    Update lab4.1.py

--- лр4/lab4.1.py
+++ лр4/lab4.1.py
@@ -2,9 +2,9 @@ import json
 
 
 def task() -> float:
-filename = 'input.json'
-with open(filename) as file:
-data = json.load(file)
-sum_dict = sum([i["score"]*i["weight"] for i in data])
-return round(sum_dict, 3)
+    filename = 'input.json'
+    with open(filename) as file:
+        data = json.load(file)
+    sum_dict = sum([i["score"]*i["weight"] for i in data])
+    return round(sum_dict, 3)
 print(task())
